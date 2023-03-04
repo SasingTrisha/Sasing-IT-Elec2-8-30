4aee2a54f27031e3b4731d2b7a2fe41f66b3c399
SasingTrisha <trishamaesasing64@gmail.com>
2023-03-04 13:11:27 +0800

    Activity2 03/04/23

--- app/src/main/java/com/example/helloandroid/MainActivity2.java
+++ app/src/main/java/com/example/helloandroid/MainActivity2.java
@@ -2,13 +2,26 @@ package com.example.helloandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+    Button button = (Button) findViewById(R.id.button4);
+
+        button.setOnClickListener(new View.OnClickListener(){
+
+    }
+
+
     }
 }
